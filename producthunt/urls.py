482b462a7048f8c12896c8a66bb2b5f2a68be33f
MohitGupta0424 <mohitgupta_0470@yahoo.com>
2019-05-29 17:48:23 -0400

    We added the Auth.

---
+++ producthunt/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+import products.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', products.views.homepage, name ='home'),
+    path('accounts/', include('accounts.urls')),
+]
